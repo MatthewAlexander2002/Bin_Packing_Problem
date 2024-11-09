162d7d8e73dc924d7775af749ca04bf2edeaae04
MatthewAlexander2002 <matthewalexander009@gmail.com>
2024-11-09 15:17:41 +1100

    set up function

---
+++ DP.cpp
@@ -0,0 +1,8 @@
+#include <vector>
+
+//minimise dp[i] to the minium number of bins needed to place all items
+
+int dynamicProgramming(const std::vector<float>& items, int binSize) {
+    std::vector<int> dp(0, items.size());
+    
+}
