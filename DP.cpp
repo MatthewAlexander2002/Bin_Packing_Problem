f61afd8bc04a981e93220056f73dad947c55d10a
MatthewAlexander2002 <matthewalexander009@gmail.com>
2024-11-09 17:39:59 +1100

    added some comments

--- DP.cpp
+++ DP.cpp
@@ -3,20 +3,29 @@
 #include <limits>
 #include <iostream>
 
+/**
+ * Solves the bin packing problem using dynamic programming with bitmasks
+ * @param items Vector of item sizes to pack
+ * @param binSize Maximum capacity of each bin
+ * @return Minimum number of bins needed to pack all items
+ */
+
 // minimise dp[i] to the minium number of bins needed to place all items
 
 int dynamicProgramming(const std::vector<float>& items, int binSize) {
     std::vector<int> dp(1 << items.size(), items.size()); //max num of bins
     dp[0] = 0; //zero items, zero bins
 
+    // iterate through all possible item subsets using bitmasks
     for (int i = 0; i < (1 << items.size()); ++i){
         std::vector<float> bins(1, binSize); // Start with one bin
 
-        //try adding each item if it hasn't already been placed
+        // pack each item that's included in current subset
         for(int j = 0; j < items.size(); ++j) {
             if (i & (1 << j)) {
                 bool packed = false;
 
+                //try putting an item into an existing bin
                 for (float& bin : bins) {
                     if (bin >= items[j]) {
                         bin -= items[j];
@@ -25,6 +34,7 @@ int dynamicProgramming(const std::vector<float>& items, int binSize) {
                     }
                 }
 
+                //create new bin if the item didn't fit
                 if (!packed) {
                     bins.push_back(binSize - items[j]);
                 }
@@ -32,9 +42,9 @@ int dynamicProgramming(const std::vector<float>& items, int binSize) {
         }
         dp[i] = bins.size();
 
-        // Try adding new items
+        // update states by trying to add each unused item
         for (int j = 0; j < items.size(); ++j) {
-            if (!(i & (1 << j))) {
+            if (!(i & (1 << j))) { // if item j not in current subset
                 dp[i | (1 << j)] = std::min(dp[i | (1 << j)], dp[i]);
             }
         }
