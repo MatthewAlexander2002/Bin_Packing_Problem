5d7876535e60ddeafd9e09fd93fbdedc5ac8ef4c
MatthewAlexander2002 <matthewalexander009@gmail.com>
2024-11-09 16:38:19 +1100

    running for the write length now

--- DP.cpp
+++ DP.cpp
@@ -1,29 +1,32 @@
 #include "DP.hpp"
 #include <vector>
+#include <limits>
 
 //minimise dp[i] to the minium number of bins needed to place all items
 
 int dynamicProgramming(const std::vector<float>& items, int binSize) {
-    std::vector<int> dp(items.size(), 0);
+    std::vector<int> dp(1 << items.size(), items.size()); //max num of bins
     dp[0] = 0; //zero items, zero bins
 
-    for (int i = 0; i < items.size(); ++i){
+    for (int i = 0; i < (1 << items.size()); ++i){
         std::vector<float> bins;
         float remainingCapacity = binSize;
 
         //try adding each item if it hasn't already been placed
         for(int j = 0; j < items.size(); ++j) {
-            if(remainingCapacity >= items[j]){
-                remainingCapacity -= items[j];
-            } else {
-                //make new bin if the item doesn't fit
-                bins.push_back(binSize - items[j]);
+            if (!(i & (1 << j))) {
+                if(remainingCapacity >= items[j]){
+                    remainingCapacity -= items[j];
+                } else {
+                    //make new bin if the item doesn't fit
+                    bins.push_back(binSize - items[j]);
+                }
             }
         }
 
         //update
-        dp[i] = std::min(dp[i], static_cast<int>(items.size()));
+        dp[i] = std::min(dp[i], static_cast<int>(bins.size()));
     }
 
-    return dp[items.size()-1];
+    return dp[(1 << items.size()) - 1];
 }
